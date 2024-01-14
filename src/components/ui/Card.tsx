4603e80f2b97f2a3f3da66464b67dcbe7a9cb66e
fatihemreyildiz <fatihemreyildiz@gmail.com>
2024-01-15 01:33:10 +0300

    Card.tsx: Card ui changed.

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,17 +1,34 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-export default function Card(props:any) {
-
-return (
-    <button onClick={props.onClick} className="flex flex-col border  text-lg w-full h-full rounded-xl shadow-xl transition ease-in-out delay-50 hover:-translate-y-3 group hover:bg-red-600 text-white ">
-        <Image width={props.width} height={props.height} className='w-full rounded-xl min-h-80 max-h-80' src={props.src} alt={props.alt} />
-        <div className='flex flex-wrap justify-between p-2 w-full '>
-        <span className='flex flex-col w-1/2'> <span className='text-red-600 group group-hover:text-white'>Collection</span> {props.name}</span>
-        <span className='flex flex-col w-1/2'> <span className='text-red-500 group group-hover:text-white'>Floor</span> {props.price}</span>
+export default function Card(props: any) {
+  return (
+    <div className="flex flex-col border text-lg w-full h-full rounded-xl shadow-xl transition ease-in-out delay-50 hover:-translate-y-3 group text-white relative">
+      <div className="flex-1">
+        <Image
+          width={props.width}
+          height={props.height}
+          className='w-full rounded-xl min-h-80 max-h-80'
+          src={props.src}
+          alt={props.alt}
+        />
+        <div className='flex flex-wrap justify-between p-2 w-full text-center'>
+          <span className='flex flex-col w-1/2'> 
+            <span className='text-red-600 group'>
+              Name
+            </span> 
+            {props.name}
+          </span>
+          <span className='flex flex-col w-1/2'> 
+            <span className='text-red-600 group'>
+              Price
+            </span> 
+            {props.price}
+          </span>
         </div>
-    </button>
-
-
-)
-
+      </div>
+      <button onClick={props.onClick} className="bg-blue-500 text-white py-2 rounded-b-xl hover:bg-red-600">
+        Envanter'e Ekle
+      </button>
+    </div>
+  );
 }
