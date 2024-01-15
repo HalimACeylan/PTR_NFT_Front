bd1f3fd1d854ffeeb295f242a9e4b1ad38a22172
Halim Ceylan <halimceylan7@gmail.com>
2024-01-15 03:38:27 +0300

    bug fix

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,71 +1,110 @@
 "use client";
-import Card from '../ui/Card'
-import { useEffect,useState } from "react";
+import Card from "../ui/Card";
+import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import MarketPlace from "../../contractsData/Marketplace.json";
 import MarketPlaceAddress from "../../contractsData/Marketplace-address.json";
-export default function Hero(props:any){
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export default function Hero(props: any) {
   const [Cards, setCards] = useState();
 
-
-  useEffect(() => { 
+  useEffect(() => {
     const items = async () => {
-      console.log(props)
-      const itemCount = await props.MarketPlaceContract.itemCount()
-      console.log(itemCount)
-      const itemsList = []
+      console.log(props);
+      const itemCount = await props.MarketPlaceContract.itemCount();
+      console.log(itemCount);
+      const itemsList = [];
       for (let i = 1; i <= itemCount; i++) {
-        const item = await props.MarketPlaceContract.items(i)
-        console.log(item)
-        const uri = await props.NFTContract.tokenURI(item.tokenId)
-        const totalPrice = await props.MarketPlaceContract.getTotalPrice(item.itemId)
-        if(!item.sold){
+        const item = await props.MarketPlaceContract.items(i);
+        console.log(item);
+        const uri = await props.NFTContract.tokenURI(item.tokenId);
+        const totalPrice = await props.MarketPlaceContract.getTotalPrice(
+          item.itemId
+        );
+        if (!item.sold) {
           itemsList.push({
             itemId: item.itemId,
             tokenId: item.tokenId,
             uri: uri,
+            seller: item.seller,
             price: totalPrice.toString(),
-          })
+          });
         }
-      } 
-      const Purchase = (item:any) => {
-       async function buy(){
-          console.log(props.MarketPlaceContract)
-          const tx = await props.MarketPlaceContract.purchaseItem(item.itemId , {value: item.price});
-          await tx.wait();
-          console.log(tx)
+      }
+      const Purchase = (item: any) => {
+        async function buy() {
+          if (item.seller.toLowerCase() === props.account.toLowerCase()) {
+            console.log("alo");
+            toast("Kendi NFT'nizi satın alamazsınız");
+            return;
+          } else {
+            const tx = await props.MarketPlaceContract.purchaseItem(
+              item.itemId,
+              { value: item.price }
+            );
+            await tx.wait();
+            toast("NFT satın alındı");
+            window.location.reload();
+            console.log(tx);
+          }
         }
         return buy;
-      }
-     const Cards = itemsList.map((item:any) => {
-      return <Card key={item.itemId} isInventory={false} onClick={Purchase(item)} src={"https://ipfs.io/ipfs/"+item.uri} alt="image" name={item.tokenId.toString} price={item.price.toString()} width="300" height="300"/>
-    }); 
-    return Cards;
-    }
-    items().then((res:any) => {
-      setCards(res)
-    })
-  },[props])
-  
+      };
+      const Cards = itemsList.map((item: any) => {
+        return (
+          <Card
+            key={item.itemId}
+            isInventory={false}
+            onClick={Purchase(item)}
+            src={"https://ipfs.io/ipfs/" + item.uri}
+            alt="image"
+            name={item.tokenId.toString}
+            price={item.price.toString()}
+            width="300"
+            height="300"
+          />
+        );
+      });
+      return Cards;
+    };
+    items().then((res: any) => {
+      setCards(res);
+    });
+  }, [props]);
 
+  const navbarText = ["Hepsi", "Tarih", "Spor", "Bilim", "Müzik"];
+  const navbar = navbarText.map((text: any) => (
+    <button
+      key={text}
+      className="text-white hover:bg-red-600 px-4 rounded-xl py-2.5 text-xl"
+    >
+      {text}
+    </button>
+  ));
 
-
-      const navbarText = ["Hepsi", "Tarih", "Spor", "Bilim", "Müzik"];
-      const navbar = navbarText.map((text:any) => (
-        <button
-          key={text}
-          className="text-white hover:bg-red-600 px-4 rounded-xl py-2.5 text-xl"
-        >
-          {text}
-        </button>
-      ));
-
-    return (
+  return (
     <div>
-            <div className="flex py-2 overflow-x-auto mx-12">{navbar}</div>
-        <div className="grid grid-cols-5 gap-6 w-full bg-gray-800 px-16 py-8 min-h-[50vh]" >
-            {Cards}
-        </div>
+      <div className="flex py-2 overflow-x-auto mx-12">{navbar}</div>
+      <div className="grid grid-cols-5 gap-6 w-full bg-gray-800 px-16 py-8 min-h-[50vh]">
+        {Cards}
+      </div>
+      <div>
+      <ToastContainer  
+          position="top-left"
+          autoClose={1000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          />
+      </div>
     </div>
-        )
+  );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,13 +15,16 @@ export default function Index() {
   const [Marketplace, setMarketplace] = useState(new ethers.Contract(MarketPlaceAddress.address, MarketPlace.abi,provider))
   const [NFT, setNFT] = useState(new ethers.Contract(NFTAddress.address, NFTAbi.abi, provider))
   const [accountCheck, setAccountCheck] = useState((window as any).ethereum._state && (window as any).ethereum._state.accounts.length > 0);
+  const [account , setAccount] = useState((window as any).ethereum._state?.accounts[0]);
 
   window.ethereum.on('accountsChanged', async function (accounts:Array<string>) {
     window.location.reload();
     console.log(accounts)
     if(accounts.length > 0){
+      const provider = new ethers.BrowserProvider(window.ethereum)
       const signer = await provider.getSigner()
-      loadContracts(signer)
+      setMarketplace(new ethers.Contract(MarketPlaceAddress.address, MarketPlace.abi, signer))
+      setNFT(new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer))
       console.log(Marketplace);
       console.log(NFT);
     }
@@ -35,15 +38,18 @@ export default function Index() {
   }, [])
 
   const initializeProvider = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    // Get provider from Metamask
-    // Set signer
-    const signer = await provider.getSigner()
-    window.ethereum.on('chainChanged', (chainId:any) => {
-      window.location.reload();
-    })
+    if ((window as any).ethereum) {
+      await (window as any).ethereum.request({ method: "eth_requestAccounts" });
+      const provider = new ethers.BrowserProvider((window as any).ethereum);
+      const signer = await provider.getSigner()
+      setAccountCheck((window as any).ethereum._state && (window as any).ethereum._state.accounts.length > 0);
+      const accounts = await provider.send("eth_requestAccounts", []);
+      const balance = await provider.getBalance(accounts[0]);
+      const block = await provider.getBlockNumber();
+      loadContracts(signer)
 
-    loadContracts(signer)
+    }
+
   }
   const loadContracts = async (signer:any) => {
     // Get deployed copies of contracts
@@ -63,6 +69,7 @@ export default function Index() {
       <Hero
         MarketPlaceContract={Marketplace}
         NFTContract={NFT}
+        account={account}
       />
     </div>
   );
